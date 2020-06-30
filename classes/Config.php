a2e94024bcbe43d2be254d69378ed6b5cacda19c
Shreyas Ikhar <40795917+shreyasikhar@users.noreply.github.com>
2020-06-30 21:13:49 +0530

    Update Config.php

--- classes/Config.php
+++ classes/Config.php
@@ -9,6 +9,6 @@
         const SMTP_HOST = 'smtp.gmail.com';
         const SMTP_PORT = 465;
         const SMTP_USER = 'enlectic@gmail.com';
-        const SMTP_PASSWORD = 'Enlectic@123';
+        const SMTP_PASSWORD = '';
     }
 ?>
